b3a102cd0950083353325f38a9af66240641df15
David <davidricardocruzjuarez@gmail.com>
2025-01-16 17:34:26 -0600

    Adaptation program with if Else moreover

---
+++ Loigca_Adivina Numero/app.js
@@ -0,0 +1,12 @@
+//Variables
+let numeroSecreto = 3;
+let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+console.log(numeroUsuario);
+
+/*Comparacion de numeros*/
+    if (numeroUsuario == numeroSecreto) {
+    alert(`Acertaste el numero ${numeroUsuario}`);
+        }
+    else{
+    alert('la condicion no se cumplio');        
+    }   
