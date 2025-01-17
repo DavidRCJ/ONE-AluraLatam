a9a69a51a53077fd4575794cd1fbeb7f49db3922
David <davidricardocruzjuarez@gmail.com>
2025-01-17 10:16:50 -0600

    Insertion conrunter and bucle

--- Loigca_Adivina Numero/app.js
+++ Loigca_Adivina Numero/app.js
@@ -1,12 +1,24 @@
-//Variables
-let numeroSecreto = 3;
-let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
-console.log(numeroUsuario);
+alert("Bienvenido al juego del número secreto");
+let intento = 0;
+let numeroSecreto = 4;
+let contador= 1;
 
-/*Comparacion de numeros*/
-    if (numeroUsuario == numeroSecreto) {
-    alert(`Acertaste el numero ${numeroUsuario}`);
+//ciclo
+while(intento != numeroSecreto){
+    // Agrega un console.log para verificar el valor de "intento" después de la entrada del usuario
+    intento = prompt("Me proposcionas un numero entre 1 y 10 ");
+    console.log(intento);
+
+    if (intento == numeroSecreto) {
+        // Agrega un console.log para verificar el valor de "numeroSecreto" cuando el jugador se equivoca
+        //console.log("Valor del número secreto:", numeroSecreto);
+        alert(`Acertaste el numero secreto es  ` + numeroSecreto +`  Numero de intentos ` + contador );
+    } else {
+        if(intento > numeroSecreto)
+            alert("El numero secrerto es menor");
+        else {
+            alert("El numero secrerto es mayor");
         }
-    else{
-    alert('la condicion no se cumplio');        
-    }   
+        contador= contador +1;
+    } 
+}
